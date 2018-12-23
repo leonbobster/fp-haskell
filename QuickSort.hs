0d4ff06060978ad3a74a7ecfbda45d3fa8678368
Leonid Bobylev <leonbobster@gmail.com>
2018-12-23 15:45:12 +0100

    quick sort

--- QuickSort.hs
+++ QuickSort.hs
@@ -2,7 +2,7 @@ module QuickSort (qsort) where
 
 qsort :: [Int] -> [Int]
 qsort [] = []
-qsort (x:xs) = qsort smaller ++ [x] ++ larger
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
     where
         smaller = [a | a <- xs, a <= x]
         larger = [b | b <- xs, b > x]
